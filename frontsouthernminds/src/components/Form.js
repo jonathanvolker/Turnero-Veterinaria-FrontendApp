928ecdf9cc798021ca309eaf8739c10225782879
jonathanvolker <joni_451@hotmail.com>
2021-11-17 18:55:07 -0300

    modificaicones form

--- frontsouthernminds/src/components/Form.js
+++ frontsouthernminds/src/components/Form.js
@@ -9,11 +9,14 @@ const Form =() => {
     const [razas, setRazas] = useState([]);
     const fechaNacimiento = date.toString().slice(0,15)
     const [menor, setMenor] =useState(false)
+    const [exacta, setExacta] =useState(true)
+    
     const [input,setInput] =useState({  
         nombre:"",
         raza:"",
         sexo:"",
-        edadAdulta:fechaNacimiento,
+        edadAdulta:date,
+        edadAprox:"",
         mes:"" || 0, 
         aprox:"false"
     })
@@ -33,28 +36,44 @@ const Form =() => {
         
         setInput({ ...input, [e.target.name]: upper
         });
+        if (e.target.name == "edadAprox"){
+            setInput({ ...input, aprox : "true" ,[e.target.name]: upper})
+            
+          
+        }
+        
+        
     }
-
-    
+    const handleCheckbox =(e)=>{
+        setMenor(!menor)
+    }
+    const handleCheckbox2 =(e)=>{
+        setExacta(!exacta)
+    }
     const handleSubmit= (e)=> {
         e.preventDefault();
       }
     return (
+        <>
         <div className="container">
          <div className="title-container">
             <h1>Aqui puede registrar a su mascota</h1>    
          </div>
          <div className="form-container">
          <form onSubmit={handleSubmit}>
+            <div className="form-group">
+           <div className="izquierda">
+             <div className="colums">
             <label className= "label">
             <br/>
                 Nombre :
                 <br/>
                 
                 <input className={!input.nombre && "inpError"} type="text" value={input.nombre} name="nombre" onChange={handleInputChange} required/>
-            </label>    
+            </label>  
+            </div>  
             <br/>
-
+            <div className="colums">
             <label className= "label">
             <br/>
             Elija la raza:
@@ -75,11 +94,13 @@ const Form =() => {
                                   
                                 </ul>
                              }
-            </label>    
+            </label>  
+            </div>  
+            <div className="colums">
             <label className= "label">
             <br/>
             Elija el sexo:
-                        <select name="menor" value={menor} onChange={handleInputChange} >
+                        <select name="sexo" value={input.sexo} onChange={handleInputChange} >
                         <option defaultValue="selected"></option>
                         <option value="nene"> nene</option> 
                         <option value="nena"> nena</option>     
@@ -93,31 +114,82 @@ const Form =() => {
                                 </ul>
                              }
           
-           </label>    
+           </label>
+           </div>
+           </div>    
             <br/>
+
+
+             <div className="center"> 
+             <div className="colums-center">               
+             <h4>  su mascota es menor de un año?</h4> 
+                <input type="checkbox" onClick={handleCheckbox} value= "true" /> 
+             </div>                
+                <br/>
+                <br/>
+                <div className="colums-center">
+                  <h4> seleccione en caso no estar seguro de la edad </h4>    
+                  <input type="checkbox" onClick={handleCheckbox2} value= "true" /> 
+                </div>
+             </div>                
+
+
+
+            <div className="derecha">
             <label className= "label">
             <br/>
-                su mascota es menor de un año? 
-                <br/>
+              
                 <br/>
 
         { !menor ?   
-             ( <>
              ( 
-                <> 
-                seleccione en caso de no saber la fecha exacta      
-              <input type="checkbox" id="aprox" name="aprox" value="true" onChange={handleInputChange} />
-       
-              <Calendar
+             
+               exacta ? (<> 
+               <div className="colums-derecha">   
+                <h4 className= "label"> indique la fecha exacta </h4>
+                </div>
+                <div className="colums-derecha">   
+                <Calendar
                   className="react-calendar"
-                  onChange={(e)=>{setDate(e)}}
+                  onChange={(e)=>{  setInput({ ...input, edadAdulta: e.toString().slice(0,15)
+                  });}}
                   value={date}
-              />
-              </>
-        )
-           : 
-          (
-              <>
+                />
+                </div>
+                  
+              </>) 
+              : (
+                <>
+                <div className="colums-derecha">   
+                <h4>  volver a fecha exacta</h4> 
+                <button  onClick={handleCheckbox2 } value= "true" >volver </button> 
+                </div>
+                <div className="colums-derecha">   
+                <h4>elija los años aproximados</h4>
+                <select name="edadAprox" value={input.edadAprox} onChange={handleInputChange } >
+                        <option defaultValue="selected"></option>
+                        <option value="1"> 1 años </option> 
+                        <option value="2"> 2 años</option>
+                        <option value="3"> 3 años</option>
+                        <option value="4"> 4 años</option>     
+                        <option value="5"> 5 años</option>
+                        <option value="6"> 6 años</option>
+                        <option value="7"> 7 años</option>
+                        <option value="8"> 8 años</option>
+                        <option value="9"> 9 años</option>
+                        <option value="10"> 10 años</option>
+                        <option value="11"> 11 años</option>
+            </select>
+                </div>
+                </>
+
+              )
+              
+              )
+              : 
+              (
+                  <>
+                        
               <div>elija los meses</div>
               <select name="mes" value={input.mes} onChange={handleInputChange} >
                         <option defaultValue="selected"></option>
@@ -135,26 +207,25 @@ const Form =() => {
             </select>
 
               </>
-          )
-              
-              </>)
-              : null
+                  
+              )
               
               }
               
             </label>    
             <br/>
            
-                       
              
             <br/>
+            </div>
+            </div>
                 <input className="butt" onClick={handleSubmit} value= "Submit" type="submit"/> 
              </form>
         </div>   
 
 
         </div>
-
+        </>
     )
 }
 
